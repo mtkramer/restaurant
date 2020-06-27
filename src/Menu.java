e699f9c1c6d1abdd1e94840204b5a4efaf69b71a
mtkramer <kramerm1@mail.usf.edu>
2020-06-27 11:31:43 -0400

    Studio 3 completed

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,40 @@
+import java.util.*;
+
 public class Menu {
 
+    private static List<MenuItem> menuItemList = new ArrayList<>();
+
     public static void main(String[] args) {
-        
+        MenuItem garden_salad = new MenuItem("Garden Salad", 8.5, "lettuce and dressing of choice");
+        menuItemList.add(garden_salad);
+        MenuItem grilled_chicken = new MenuItem("Grilled Chicken", 11.0, "grilled chicken");
+        menuItemList.add(grilled_chicken);
+        MenuItem brownie = new MenuItem("Brownie", 5.5, "brownie with or without walnuts");
+        menuItemList.add(brownie);
+
+        printMenu(menuItemList);
+    }
+
+    private static void printMenu(List<MenuItem> menu){
+        for(MenuItem item : menu){
+            System.out.println("*****");
+            System.out.println(item.getName());
+            System.out.println("--------------------");
+            System.out.println("$" + item.getPrice() + "0");
+            System.out.println("Our best " + item.getDescription() + " made to order.");
+            System.out.println("*****");
+        }
     }
 
 }
+
+/*
+The menu consists of several menu items
+Each menu item has a price, description, and category (appetizer, main course, or dessert)
+It should be possible to display whether a menu item is new or not
+The app should know when the menu was last updated, so visitors can see that the restaurant
+is constantly changing and adding exciting new items
+In IntelliJ, create a new project, Restaurant Menu. Within the project, create a new package
+named restaurant. Add the Menu and MenuItem classes and code the design that you created above.
+Be sure to add getters and setters as appropriate.
+ */
--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,2 +1,39 @@
 public class MenuItem {
+
+    private String name;
+    private double price;
+    private String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println("Name on " + this.name + " was changed");
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        System.out.println("Price on " + this.name + " was changed");
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        System.out.println("Description on " + this.name + " was changed");
+        this.description = description;
+    }
 }
