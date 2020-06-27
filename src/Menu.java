55ab4e71b18f8e7fc947c9fcb138b71184d7c046
mtkramer <kramerm1@mail.usf.edu>
2020-06-27 11:33:02 -0400

    removed notes

--- src/Menu.java
+++ src/Menu.java
@@ -27,14 +27,3 @@ public class Menu {
     }
 
 }
-
-/*
-The menu consists of several menu items
-Each menu item has a price, description, and category (appetizer, main course, or dessert)
-It should be possible to display whether a menu item is new or not
-The app should know when the menu was last updated, so visitors can see that the restaurant
-is constantly changing and adding exciting new items
-In IntelliJ, create a new project, Restaurant Menu. Within the project, create a new package
-named restaurant. Add the Menu and MenuItem classes and code the design that you created above.
-Be sure to add getters and setters as appropriate.
- */
